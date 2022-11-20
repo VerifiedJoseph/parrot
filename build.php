3590a2e58662368344f8538e7f9d6b4af9b34aa3
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-11-20 18:46:55 +0000

    Update build.php

--- build.php
+++ build.php
@@ -4,11 +4,16 @@
  * Script for building the single file version
  */
 $files = [
-	'css' => 'public/style.css',
-	'jszip' => 'public/js/jszip.min.js',
-	'papaparse' => 'public/js/papaparse.min.js',
-	'script' => 'public/js/script.js' 
+	'css' => 'style.css',
+	'jszip' => 'js/jszip.min.js',
+	'papaparse' => 'js/papaparse.min.js',
+	'script' => 'js/script.js'
 ];
+
+function output(string $text)
+{
+	echo $text . " \n";
+}
 
 function loadfile($path): string
 {
@@ -21,10 +26,10 @@ function loadfile($path): string
 	return $page;
 }
 
-function addParts($page, $files): string
+function addInlineParts($page, $files): string
 {
 	foreach ($files as $id => $file) {
-		$data = loadfile($file);
+		$data = loadfile('public/' . $file);
 
 		if ($id === 'css') {
 			$data = '<style>' . $data . '</style>';
@@ -39,22 +44,54 @@ function addParts($page, $files): string
 	return $page;
 }
 
-function save($page)
+function addExternalParts($page, $files): string
 {
-	$status = file_put_contents('public/twMediaViewer.html', $page);
+	foreach ($files as $id => $file) {
+		if ($id === 'css') {
+			$data = '<link rel="stylesheet" type="text/css" href="'. $file .'" />';
+
+		} else {
+			$data = '<script src="'. $file .'" type="text/javascript"></script>';
+		}
+
+		$page = str_replace('{'. $id .'}', $data, $page);
+	}
+
+	return $page;
+}
+
+function addBuildDate(string $page): string
+{
+	$page = str_replace('{date}', date('c'), $page);
+	return $page;
+}
+
+function save(string $page, string $path)
+{
+	$status = file_put_contents($path, $page);
 
 	if ($status === false) {
-		throw new Exception('Failed to save file: public/twMediaViewer.html');
+		throw new Exception('Failed to save file: ' . $path);
 	}
+
+	output('Create file: ' . $path);
 }
 
 try {
+	// All one version
 	$page = loadfile('templates/twMediaViewer.html');
-	$page = addParts($page, $files);
+	$page = addInlineParts($page, $files);
+	$page = addBuildDate($page);
+	save($page, 'public/twMediaViewer.html');
 
-	save($page);
+	// Main website version
+	$page = loadfile('templates/twMediaViewer.html');
+	$page = addExternalParts($page, $files);
+	$page = addBuildDate($page);
+	save($page, 'public/index.html');
 
+	output('Done.');
 } catch(Exception $e) {
-	echo $e->getMessage();
+	output($e->getMessage());
 	exit(1);
 }
