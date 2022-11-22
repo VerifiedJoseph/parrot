69effd8313c9b023a05c1615ddaa1dc285f363b3
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-11-22 00:20:43 +0000

    Create dom.js

---
+++ public/js/dom.js
@@ -0,0 +1,34 @@
+function displayError(text) {
+    innerText('error', text);
+    show('error');
+} 
+
+function hideError() {
+    innerText('error', '');
+    hide('error');
+} 
+
+function clearTweets() {
+    hide('tweets');
+    innerText('tweets', '');
+}
+
+function show(element) {
+    document.getElementById(element).classList.remove('hide');
+}
+
+function hide(element) {
+    document.getElementById(element).classList.add('hide');
+}
+
+function disableInput(element) {
+    document.getElementById(element).disabled = true;
+}
+
+function enableInput(element) {
+    document.getElementById(element).disabled = false;
+}
+
+function innerText(element, text) {
+    document.getElementById(element).innerText = text;
+}
