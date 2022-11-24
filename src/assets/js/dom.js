d2d7ba82702de634170f79e061b1746b93b564f2
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-11-24 19:31:13 +0000

    Create dom.js

---
+++ src/assets/js/dom.js
@@ -0,0 +1,76 @@
+/**
+ * Control DOM elements
+ */
+export const dom = {
+  /**
+   * Display error message
+   *
+   * @param {string} text
+   */
+  displayError: function (text) {
+    this.innerText('error', text)
+    this.show('error')
+  },
+
+  /**
+   * Hide error message
+   */
+  hideError: function () {
+    this.innerText('error', '')
+    this.hide('error')
+  },
+
+  /**
+   * Clear tweets
+   */
+  clearTweets: function () {
+    this.hide('tweets')
+    this.innerText('tweets', '')
+  },
+
+  /**
+   * Show an element
+   *
+   * @param {string} element Element id
+   */
+  show: function (element) {
+    document.getElementById(element).classList.remove('hide')
+  },
+
+  /**
+   * Hide an element
+   *
+   * @param {string} element Element id
+   */
+  hide: function (element) {
+    document.getElementById(element).classList.add('hide')
+  },
+
+  /**
+   * Enable an input
+   *
+   * @param {string} element Element id
+   */
+  enableInput: function (element) {
+    document.getElementById(element).disabled = false
+  },
+
+  /**
+   * Disable an input
+   *
+   * @param {string} element Element id
+   */
+  disableInput: function (element) {
+    document.getElementById(element).disabled = true
+  },
+
+  /**
+   * Set inner text for element
+   *
+   * @param {string} element Element id
+   * @param {string} text Text
+   */
+  innerText: function (element, text) {
+    document.getElementById(element).innerText = text
+  }
+}
