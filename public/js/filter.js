c5705ab6f7540a585c68ddf6296daa4211041402
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-11-24 12:30:41 +0000

    Update filter.js

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,69 +1,75 @@
-/**
- * Filter tweets
- */
-function filter (data) {
-  const userFilter = document.getElementById('username-filter').value
-  const mediaFilter = document.getElementById('media-filter').value
+/* global dom, helper */
 
-  const elements = document.querySelectorAll('div.tweet')
-  elements.forEach(function (element) {
-    element.classList.add('hide')
-  })
+const filter = { // eslint-disable-line no-unused-vars
+  /**
+   * Filter tweets
+   *
+   * @param {array} data
+   */
+  run: function (data) {
+    const userFilter = document.getElementById('username-filter').value
+    const mediaFilter = document.getElementById('media-filter').value
 
-  const filtered = data.tweets.filter(function (tweet) {
-    if (userFilter !== 'all' && tweet.username !== userFilter) {
-      return false
-    }
+    const elements = document.querySelectorAll('div.tweet')
+    elements.forEach(function (element) {
+      element.classList.add('hide')
+    })
 
-    if (mediaFilter !== 'all') {
-      switch (mediaFilter) {
-        case 'none':
-          if (tweet.media.length > 0) {
-            return false
-          }
-          break
-        case 'videos':
-          if (tweet.stats.videos === 0) {
-            return false
-          }
-          break
-        default:
-          if (tweet.stats.images === 0) {
-            return false
-          }
-          break
+    const filtered = data.tweets.filter(function (tweet) {
+      if (userFilter !== 'all' && tweet.username !== userFilter) {
+        return false
       }
+
+      if (mediaFilter !== 'all') {
+        switch (mediaFilter) {
+          case 'none':
+            if (tweet.media.length > 0) {
+              return false
+            }
+            break
+          case 'videos':
+            if (tweet.stats.videos === 0) {
+              return false
+            }
+            break
+          default:
+            if (tweet.stats.images === 0) {
+              return false
+            }
+            break
+        }
+      }
+
+      const element = document.querySelector('div[data-id="' + tweet.id + '"]')
+      element.classList.remove('hide')
+
+      return true
+    })
+
+    dom.innerText('username-filter-number', helper.formatNumber(filtered.length))
+
+    let name = userFilter
+    if (userFilter !== 'none') {
+      if (userFilter === 'all') {
+        name = helper.formatNumber(data.stats.users) + ' users'
+      }
+
+      dom.innerText('username-filter-name', name)
     }
+  },
 
-    const element = document.querySelector('div[data-id="' + tweet.id + '"]')
-    element.classList.remove('hide')
+  /**
+   * Remove username options from user filter list
+   */
+  clearUsernames: function () {
+    const f = document.getElementById('username-filter')
 
-    return true
-  })
+    f.querySelectorAll('option').forEach(function (element, index) {
+      if (index !== 0) {
+        element.parentNode.removeChild(element)
+      }
+    })
 
-  innerText('username-filter-number', formatNumber(filtered.length))
-
-  let name = userFilter
-  if (userFilter !== 'none') {
-    if (userFilter === 'all') {
-      name = formatNumber(data.stats.users) + ' users'
-    }
-
-    innerText('username-filter-name', name)
+    f.getElementsByTagName('option')[0].selected = 'selected'
   }
-}
-
-/**
- * Remove username options from user filter list
- */
-function clearFilterList () {
-  const filter = document.getElementById('username-filter')
-
-  filter.querySelectorAll('option').forEach(function (element, index) {
-    if (index !== 0) {
-      element.parentNode.removeChild(element)
-    }
-  })
-
-  filter.getElementsByTagName('option')[0].selected = 'selected'
 }
