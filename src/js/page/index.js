5ec66278e89ee69dd294fd2a54614c5c384765b7
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-12-06 17:25:49 +0000

    Update index.js

--- src/js/page/index.js
+++ src/js/page/index.js
@@ -171,6 +171,9 @@ function loadFile (input) {
 
         Dom.enableInput('media-filter-reset')
         Dom.enableInput('media-filter')
+        Dom.enableInput('search-input')
+        Dom.enableInput('search')
+        Dom.enableInput('search-reset')
 
         document.getElementById('username-filter').addEventListener('change', function (e) {
           Filter.run(data)
@@ -182,6 +185,15 @@ function loadFile (input) {
 
         document.getElementById('username-filter-reset').addEventListener('click', function (e) {
           document.getElementById('username-filter').getElementsByTagName('option')[0].selected = 'selected'
+          Filter.run(data)
+        })
+
+        document.getElementById('search').addEventListener('click', function (e) {
+          Filter.run(data)
+        })
+
+        document.getElementById('search-reset').addEventListener('click', function (e) {
+          document.getElementById('search-input').value = ''
           Filter.run(data)
         })
 
@@ -222,11 +234,14 @@ function loadFile (input) {
 document.getElementById('close-file').addEventListener('click', function (e) {
   document.getElementById('zip-file').value = ''
 
-  Dom.hide('username-filter-text')
+  Dom.hide('filter-text')
   Dom.disableInput('username-filter-reset')
   Dom.disableInput('username-filter')
   Dom.disableInput('media-filter-reset')
   Dom.disableInput('media-filter')
+  Dom.disableInput('search-input')
+  Dom.disableInput('search')
+  Dom.disableInput('search-reset')
 
   Dom.disableInput('close-file')
   Dom.clearTweets()
