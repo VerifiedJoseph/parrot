72b692bbecd6a950d6d612086b904da8032db3ee
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-06-05 15:54:53 +0100

    Update Dom.js

--- src/js/Dom.js
+++ src/js/Dom.js
@@ -1,79 +1,79 @@
 /**
  * Control DOM elements
  */
-export const Dom = {
+export class Dom {
   /**
    * Display error message
    * @param {string} text
    */
-  displayError: function (text) {
+  static displayError (text) {
     this.innerText('error', text)
     this.show('error')
-  },
+  }
 
   /**
    * Hide error message
    */
-  hideError: function () {
+  static hideError () {
     this.innerText('error', '')
     this.hide('error')
-  },
+  }
 
   /**
    * Clear tweets
    */
-  clearTweets: function () {
+  static clearTweets () {
     this.hide('tweets')
     this.innerText('tweets', '')
-  },
+  }
 
   /**
    * Show an element
    * @param {string} element Element id
    */
-  show: function (element) {
+  static show (element) {
     document.getElementById(element).classList.remove('hide')
-  },
+  }
 
   /**
    * Hide an element
    * @param {string} element Element id
    */
-  hide: function (element) {
+  static hide (element) {
     document.getElementById(element).classList.add('hide')
-  },
+  }
 
   /**
    * Enable an input
    * @param {string} element Element id
    */
-  enableInput: function (element) {
+  static enableInput (element) {
     document.getElementById(element).disabled = false
-  },
+  }
 
   /**
    * Disable an input
    * @param {string} element Element id
    */
-  disableInput: function (element) {
+  static disableInput (element) {
     document.getElementById(element).disabled = true
-  },
+  }
 
   /**
    * Set inner text for element
    * @param {string} element Element id
    * @param {string} text Text
    */
-  innerText: function (element, text) {
+  static innerText (element, text) {
     document.getElementById(element).innerText = text
-  },
+  }
 
   /**
    * Set title for element
    * @param {string} element Element id
    * @param {string} text Text
    */
-  title: function (element, text) {
+  static title (element, text) {
     document.getElementById(element).title = text
   }
 }
